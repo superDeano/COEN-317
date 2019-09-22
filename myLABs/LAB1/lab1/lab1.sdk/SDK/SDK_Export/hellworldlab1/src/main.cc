99385c239607ea01669af5fa33715c2782c9f8b2
Dean <deancs1997@gmail.com>
2019-09-22 11:13:27 -0400

    Files for my first lab

---
+++ myLABs/LAB1/lab1/lab1.sdk/SDK/SDK_Export/hellworldlab1/src/main.cc
@@ -0,0 +1,17 @@
+/*
+ * Empty C++ Application
+ * Created by Dean Chong San
+ */
+
+#include <iostream>
+void print();
+
+int main()
+{
+	print();
+	return 0;
+}
+
+void print(){
+	std::cout << "Hello World my friend, by Dean" <<std::endl;
+}
